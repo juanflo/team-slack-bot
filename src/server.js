14453a2c66fd5f25dde180cc8c3790b8af7548a4
Juan Flores <juan.flores@libertymutual.com>
2018-06-05 09:46:39 -0400

    add a couple of handlers

--- src/server.js
+++ src/server.js
@@ -1,12 +1,27 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const url = require('url');
+const request = require('request');
 
 
 const app = express();
 const port = process.env.PORT || 3005;
 
+const API = '/slack';
+
 app.use(bodyParser.json());
-app.get('/', (req, res) => res.status(200).send('Hello slack!'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get(API, (req, res) => res.status(200).send('Hello slack!'));
+
+app.post(`${API}/random-facilitator`, (req, res) => {
+    console.log(req);
+    res.status(200).send('/random-facilitator post');
+});
+
+app.post(`${API}/schedule`, (req, res) => {
+
+});
 
 app.use((err, req, res, next) => {
     console.log(err.stack);
