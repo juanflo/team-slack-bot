ae30f96dcd3c9d362d0edffa5bf61b4743d9b9d3
Juan Flores <juan.flores@libertymutual.com>
2018-06-15 13:21:41 -0400

    frequency selection response api

--- src/server.js
+++ src/server.js
@@ -44,10 +44,10 @@ console.log('/frequency');
 
     
     console.log(`callback_id: ${callback_id}, channel_id: ${channel_id}, user_id: ${user_id}`);
-    console.log(payload.actions[0].selected_options[0].value);
+    console.log(payload.actions[0].selected_options.value);
 
     switch(callback_id) {
-        case 'randy_frequency': _saveScheduling(user_id, channel_id, 'frequency', payload.actions[0].selected_options[0].value);
+        case 'randy_frequency': _saveScheduling(user_id, channel_id, 'frequency', payload.actions[0].selected_options.value);
     }
 
     if (callback_id === CALLBACK_ID.FREQUENCY) {
