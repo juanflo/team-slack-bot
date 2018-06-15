765278fb4465228d4cb0070b1dbfef7d70d79880
Juan Flores <juan.flores@libertymutual.com>
2018-06-15 09:49:10 -0400

    frequency response

--- src/server.js
+++ src/server.js
@@ -16,7 +16,8 @@ app.get(API, (req, res) => res.status(200).send('Hello slack!'));
 
 app.post(`${API}/random-facilitator`, (req, res) => {
     console.log(req);
-    res.status(200).send('/random-facilitator post');
+    res.set('Content-Type', 'application/json');
+    res.status(200).send(getFrequencyMessage());
 });
 
 app.post(`${API}/schedule`, (req, res) => {
@@ -27,5 +28,47 @@ app.use((err, req, res, next) => {
     console.log(err.stack);
     res.status(400).send(err.message);
 });
+
+function getFrequencyMessage() {
+    return {
+        'text': 'Lets schedule a facilitator!',
+        'attachments': [
+            {
+                'text': 'How frequent do you want to me to choose someone?',
+                'fallback': 'You are unable to schedule a facilitator',
+                'callback_id': 'wopr_game',
+                'color': '#3AA3E3',
+                'attachment_type': 'default',
+                'actions': [
+                    {
+                        'name': 'frequncy',
+                        'text': '1 minute',
+                        'type': 'button',
+                        'value': 'one-minute'
+                    },
+                    {
+                        'name': 'frequncy',
+                        'text': 'Daily',
+                        'type': 'button',
+                        'value': 'daily'
+                    },
+                    {
+                        'name': 'frequncy',
+                        'text': 'Weekly',
+                        'type': 'button',
+                        'value': 'weekly'
+                    },
+                                    {
+                        'name': 'frequncy',
+                        'text': 'Cancel',
+                        'type': 'button',
+                        'value': 'cancel',
+                        'style': 'danger'
+                    }
+                ]
+            }
+        ]
+    };
+}
 
 app.listen(port, () => console.log(`bot listening on port ${port}`));
