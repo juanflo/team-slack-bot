2c631f41b6013c2bed499e4c1380c281661cb7fa
Juan Flores <juan.flores@libertymutual.com>
2018-06-15 10:48:14 -0400

    frequency selection response api

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const url = require('url');
 const request = require('request');
+const interactiveResponse = require('interactiveResponse');
 
 
 const app = express();
@@ -16,11 +17,12 @@ app.get(API, (req, res) => res.status(200).send('Hello slack!'));
 
 app.post(`${API}/random-facilitator`, (req, res) => {
     res.set('Content-Type', 'application/json');
-    res.status(200).send(getFrequencyMessage());
+    res.status(200).send(interactiveResponse.getFrequencyMessage());
 });
 
 app.post(`${API}/frequency`, (req, res) => {
     console.log(req);
+    res.status(200).send(interactiveResponse.getDailyResponse());
 });
 
 app.post(`${API}/schedule`, (req, res) => {
@@ -31,47 +33,5 @@ app.use((err, req, res, next) => {
     console.log(err.stack);
     res.status(400).send(err.message);
 });
-
-function getFrequencyMessage() {
-    return {
-        'text': 'Lets schedule a facilitator!',
-        'attachments': [
-            {
-                'text': 'How frequent do you want to me to choose someone?',
-                'fallback': 'You are unable to schedule a facilitator',
-                'callback_id': 'wopr_game',
-                'color': '#3AA3E3',
-                'attachment_type': 'default',
-                'actions': [
-                    {
-                        'name': 'frequncy',
-                        'text': '1 minute',
-                        'type': 'button',
-                        'value': 'one-minute'
-                    },
-                    {
-                        'name': 'frequncy',
-                        'text': 'Daily',
-                        'type': 'button',
-                        'value': 'daily'
-                    },
-                    {
-                        'name': 'frequncy',
-                        'text': 'Weekly',
-                        'type': 'button',
-                        'value': 'weekly'
-                    },
-                                    {
-                        'name': 'frequncy',
-                        'text': 'Cancel',
-                        'type': 'button',
-                        'value': 'cancel',
-                        'style': 'danger'
-                    }
-                ]
-            }
-        ]
-    };
-}
 
 app.listen(port, () => console.log(`bot listening on port ${port}`));
